0fd14fd1eed300652672dca3752d68971d36ba27
FarziBuilder <faraaz@elixarsystems.com>
2023-01-12 15:59:45 +0530

    LessGo
    Boom Boom!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const configuration = new Configuration({
+    apiKey: "",
+  });
+
+  const openai = new OpenAIApi(configuration);
+  const [result,setResult] = useState("");
+  const generateImage = async () => {
+  const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+    });
+    setResult(res.data.data[0].url);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-main">
+      <div className="app-container">
+        <h2 className = "header-text">Type out ur dream</h2>
+
+        <textarea
+          className="app-input"
+          placeholder="Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
+          onChange={(e) => setPrompt(e.target.value)}
+          rows="10"
+          cols="40"
+        />
+        <button className="black-button" onClick={generateImage}>Generate an Image</button>
+        {result.length > 0 ? (
+          <img className="result-image" src={result} alt="result" />
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
